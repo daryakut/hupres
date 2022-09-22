7257488dca458b439665c15c611643a9e764c4a8
Georgii Oleinikov <olgeorge@gmail.com>
2022-09-22 04:18:17 +0200

    0auth attempt

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,104 @@
 """
 Application definition
 """
+import json
+import os
 
+from authlib.integrations.starlette_client import OAuth
+from authlib.oauth2 import OAuth2Client
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
-
-from sqlalchemy import create_engine, Column, Integer, String, Sequence, func
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import func
+from starlette.middleware.sessions import SessionMiddleware
+from starlette.requests import Request
 
 from database.db_models import DbUser
-from database.factories import create_user
 from database.transaction import transaction
 from quiz_algorithm.models import Quiz, User, UserRole
 
+from authlib.integrations.starlette_client import OAuth
+from authlib.integrations.base_client.async_openid import AsyncOpenIDMixin
+
+from authlib.oidc.core import IDToken
+
+from authlib.oauth2.rfc6749.wrappers import OAuth2Token
+
+
+class CustomOAuth2Token(OAuth2Token):
+    def validate_iss(self, iss):
+        print("=========== validate_iss")
+        # Skip iss check or customize the validation logic here
+        pass
+
+
+# class CustomIDToken(IDToken):
+#
+#     def validate_iss(self):
+#         print("=========== validate_iss")
+#         # Override to suppress iss check
+#         pass
+
+
+oauth = OAuth()
+oauth.register(
+    name='google',
+    issuer='https://accounts.google.com',
+    iss='https://accounts.google.com',
+    client_id=os.environ['HUPRES_GOOGLE_0AUTH_CLIENT_ID'],
+    client_secret=os.environ['HUPRES_GOOGLE_0AUTH_CLIENT_SECRET'],
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    refresh_token_url=None,
+    redirect_uri='https://f4d9-2607-fea8-86e5-fc00-2457-6377-7d54-1aff.ngrok-free.app/auth',
+    client_kwargs={'scope': 'openid profile email'},
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    # client_kwargs={
+    #     'scope': 'openid profile email',
+    #     'token_endpoint_auth_method': 'client_secret_basic',
+    #     'id_token_cls': CustomIDToken  # Use the custom ID token class
+    # }
+)
+
 app = FastAPI()
+
+# This is important for OAuth2 to store temporary state
+app.add_middleware(SessionMiddleware, secret_key=os.environ['HUPRES_SECRET_SESSION_KEY'])
+
+
+@app.get("/login")
+async def login(request: Request):
+    redirect_uri = request.url_for('auth')
+    return await oauth.google.authorize_redirect(request, redirect_uri)
+
+
+@app.get("/auth")
+async def auth(request: Request):
+    print("request", request.__dir__())
+
+    token = await oauth.google.authorize_access_token(request)
+    print(f"token {token}")
+
+    # Fetch the user's profile
+    user = await oauth.google.parse_id_token(request, token)
+    print(f"user {user}")
+
+    # Here you can create a user session or do whatever you'd like
+    return {"email": user['email'], "name": user['name']}
+
+
+@app.get("/set-session")
+def set_session(request: Request):
+    request.session["message"] = "Hello, World!"
+    return {"status": "session set"}
+
+
+@app.get("/get-session")
+def get_session(request: Request):
+    return {"session_value": request.session.get("message")}
+
 
 # # Base = declarative_base()
 #
@@ -56,7 +139,6 @@ class CreateQuizResponse(BaseModel):
 
 @app.post("/quizzes")
 async def create_quiz() -> CreateQuizResponse:
-
     return CreateQuizResponse(quiz=Quiz())
 
 
@@ -73,3 +155,8 @@ async def user_signup(request: UserSignupRequest) -> UserSignupResponse:
     with transaction() as session:
         db_user = DbUser.create_user(session, email_address=request.email_address, role=UserRole.RESPONDENT)
         return UserSignupResponse(user=db_user.to_model())
+
+
+@app.post("/auth/callback")
+async def auth_callback(request):
+    print(f"Received 0Auth callback: {json.dumps(request)}")
