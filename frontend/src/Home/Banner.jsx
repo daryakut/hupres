9acf5a582d343d71bec45f26b44a916daa0c170a
Daria Kutluieva <daryakutlueva@gmail.com>
2022-11-05 19:19:29 +0100

    Switch frontend to ant-design

---
+++ frontend/src/Home/Banner.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ScrollElement from 'rc-scroll-anim/lib/ScrollElement';
+import GitHubButton from 'react-github-button';
+import {Button, Icon} from 'antd';
+import QueueAnim from 'rc-queue-anim';
+
+function typeFunc(a) {
+  if (a.key === 'line') {
+    return 'right';
+  } else if (a.key === 'button') {
+    return 'bottom';
+  }
+  return 'left';
+}
+
+export default function Banner({ onEnterChange }) {
+  return (
+    <section className="page banner-wrapper">
+      <ScrollElement
+        className="page"
+        id="banner"
+        onChange={({ mode }) => onEnterChange(mode)}
+        playScale={0.9}
+      >
+        <QueueAnim className="banner-text-wrapper" type={typeFunc} delay={300} key="banner">
+          <h2 key="h2">РОЗУМІННЯ СЕБЕ ТА ІНШИХ РОБИТЬ ЖИТТЯ ПРОСТІШИМ ТА БІЛЬШ КОМФОРТНИМ</h2>
+          <h5 key="h5">НАШ УНІКАЛЬНИЙ МЕТОД ВИЗНАЧАЄ ХАРАКТЕР ЛЮДИНИ НА ОСНОВІ БУДОВИ ТІЛА</h5>
+          <Button key="start-test" size={20}>Пройти тест</Button>
+        </QueueAnim>
+        <Icon type="down" className="down" />
+      </ScrollElement>
+    </section>
+  );
+}
+Banner.propTypes = {
+  onEnterChange: PropTypes.func,
+};
---
+++ frontend/src/Home/Header.jsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { Menu, Row, Col, Icon, Button, Popover, Badge } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+
+const searchEngine = 'Google';
+
+export default class Header extends React.Component {
+  static propTypes = {
+    isFirstScreen: PropTypes.bool,
+    isMoblie: PropTypes.bool,
+  }
+  state = {
+    menuVisible: false,
+  };
+  onMenuVisibleChange = (visible) => {
+    this.setState({
+      menuVisible: visible,
+    });
+  }
+  handleShowMenu = () => {
+    this.setState({
+      menuVisible: true,
+    });
+  }
+
+  handleHideMenu = () => {
+    this.setState({
+      menuVisible: false,
+    });
+  }
+
+  handleSelectFilter = (value, option) => {
+    const optionValue = option.props['data-label'];
+    return optionValue === searchEngine ||
+      optionValue.indexOf(value.toLowerCase()) > -1;
+  }
+
+  render() {
+    const { isFirstScreen, isMoblie } = this.props;
+    const { menuVisible } = this.state;
+    const menuMode = isMoblie ? 'inline' : 'horizontal';
+    const headerClassName = classNames({
+      clearfix: true,
+      'home-nav-white': !isFirstScreen,
+    });
+
+    const menu = [
+      // <Button className="header-lang-button" ghost size="small" key="lang">
+      //   English
+      // </Button>,
+      <Menu mode={menuMode} defaultSelectedKeys={['home']} id="nav" key="nav">
+        <Menu.Item key="home">
+          ЩО ТАКЕ HUPRES
+        </Menu.Item>
+        <Menu.Item key="docs/spec">
+          ПРАКТИЧНЕ ВИКОРИСТАННЯ
+        </Menu.Item>
+        <Menu.Item key="docs/pattern">
+          ПРОДУКТИ
+        </Menu.Item>
+        <Menu.Item key="docs/react">
+          СТРАТЕГІЯ РОЗВИТКУ
+        </Menu.Item>
+      </Menu>,
+    ];
+
+    return (
+      <header id="header" className={headerClassName}>
+        {menuMode === 'inline' ? (
+          <Popover
+            overlayClassName="popover-menu"
+            placement="bottomRight"
+            content={menu}
+            trigger="click"
+            visible={menuVisible}
+            arrowPointAtCenter
+            onVisibleChange={this.onMenuVisibleChange}
+          >
+            <Icon
+              className="nav-phone-icon"
+              type="menu"
+              onClick={this.handleShowMenu}
+            />
+          </Popover>
+        ) : null}
+        <Row>
+          <Col lg={4} md={5} sm={22} xs={22}>
+            <a id="logo">
+              <img alt="logo" src="https://hupres.com/image/catalog/logo.svg" />
+            </a>
+          </Col>
+          <Col lg={18} md={17} sm={0} xs={0}>
+            {menuMode === 'horizontal' ? menu : null}
+          </Col>
+          <Col lg={2} md={2} sm={2} xs={2}>
+            <UserOutlined key="profile" style={{ fontSize: '26px', color: '#ddd', margin: 20, cursor: "pointer" }}/>
+          </Col>
+        </Row>
+      </header>
+    );
+  }
+}
