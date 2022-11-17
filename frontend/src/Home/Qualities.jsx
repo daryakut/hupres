e1f8fb049e3e417004334fac612c4617e662f975
Daria Kutluieva <daryakutlueva@gmail.com>
2022-11-17 03:47:44 +0100

    Qualities page

--- frontend/src/Home/Qualities.jsx
+++ frontend/src/Home/Qualities.jsx
@@ -4,74 +4,59 @@ import ScrollOverPack from 'rc-scroll-anim/lib/ScrollOverPack';
 import QueueAnim from 'rc-queue-anim';
 import {Col, Row} from "antd";
 
+const BackgroundIcon = ({className, caption, description}) => (
+  <div className="qualities-single-icon-container">
+    <div className={`qualities-bg-icon ${className}`}/>
+    <p className="landing-font-md color-blue text-align-center">
+      {caption}
+    </p>
+    <p className="landing-font-sm color-blue text-align-center">
+      {description}
+    </p>
+  </div>
+);
+
 export default function Qualities({isMobile}) {
   return (
     <>
-      <div className="my-page">
-        {/*<ScrollOverPack>*/}
-          <Row>
-            <Col xs={1} sm={2} md={4} lg={6} xl={7}/>
-            <Col xs={22} sm={20} md={16} lg={12} xl={10}>
-              <div className="text-wrapper page1-content">
-              {/*<QueueAnim*/}
-              {/*  // type={isMobile ? 'bottom' : 'right'}*/}
-              {/*  type={'left'}*/}
-              {/*  className="text-wrapper page1-content-1"*/}
-              {/*  key="text1"*/}
-              {/*  leaveReverse*/}
-              {/*>*/}
-                <hr key="hr1" className="landing-hr"/>
-                <p key="p1.1" className="landing-font-md">
-                  Консультант-психолог за допомогою штучного інтелекту на основі технології HUPRES дає вам змогу
-                  без проходження психологічних тестів отримати в режимі чату психологічну консультацію про себе та
-                  інших людей з будь-яких питань.
-                </p>
-                <p key="p.2" className="landing-font-md">
-                  Для отримання вірної інформації про характер людини технологія HUPRES використовує лише ваші
-                  знання про її зовнішній вигляд.
-                </p>
-              {/*</QueueAnim>*/}
-                </div>
-            </Col>
-          </Row>
-        {/*</ScrollOverPack>*/}
-        {/*<ScrollOverPack>*/}
-          <Row>
-            <Col xs={1} sm={2} md={4} lg={6} xl={7}/>
-            <Col xs={22} sm={20} md={16} lg={12} xl={10}>
-
-              {/*<QueueAnim*/}
-              {/*  // type={isMobile ? 'bottom' : 'right'}*/}
-              {/*  type={'left'}*/}
-              {/*  className="text-wrapper page1-content"*/}
-              {/*  key="text1"*/}
-              {/*  leaveReverse*/}
-              {/*>*/}
-              <div className="text-wrapper page1-content">
-                <hr key="hr2" className="landing-hr"/>
-                <h2 key="h2.2" className="landing-font-xl">ПРАКТИЧНЕ ЗАСТОСУВАННЯ</h2>
-                <div key="p2.2" className="how-it-works-ul-wrapper">
-                  <p key="p2.3" className="landing-font-sm">
-                    Завдяки <strong>Human Personal Recognition System</strong> або HUPRES про людину можна
-                    дізнатись дуже багато, використовуючи лише його фотографії або відео.
-                  </p>
-                  <p key="p2.4" className="landing-font-sm">
-                    Більш того, HUPRES допомагає не тільки у пізнанні іншої людини, але й самого себе. Далеко не
-                    кожен з нас здатен тверезо оцінити свої здібності та таланти.
-                  </p>
-                  <p key="p2.5" className="landing-font-sm">
-                    У пошуках ідеального для себе образу життя, роботи, партнера або стосунків ми часто
-                    помиляємося, підлаштовуючі себе під стереотипи, що нам зовсім невластиві.
-                  </p>
-                  <p key="p2.6" className="landing-font-sm">
-                    Аналіз HUPRES дає конкретний та чіткий мануал дій, що оберігає від помилок.
-                  </p>
-                </div>
-                </div>
-              {/*</QueueAnim>*/}
-            </Col>
-          </Row>
-        {/*</ScrollOverPack>*/}
+      <div className="qualities-page">
+        <Row>
+          <Col xs={1} sm={2} md={4} lg={6} xl={7}/>
+          <Col xs={22} sm={20} md={16} lg={12} xl={10}>
+            <h5 key="p.2" className="landing-font-lg color-blue">
+              НА ОСНОВІ СОМАТИЧНИХ (ТІЛЕСНИХ) ОЗНАК МЕТОДИКА ВИЗНАЧАЄ:
+            </h5>
+          </Col>
+        </Row>
+        <div className="qualities-four-icons-container">
+          <div className="qualities-icon-pair-container">
+            <BackgroundIcon
+              className="bg-mozg_13"
+              caption="СИЛЬНІ ТА СЛАБКІ СТОРОНИ ОСОБИСТОСТІ"
+              description={"УСЕБІЧНО РОЗВИНЕНИХ ЛЮДЕЙ НЕ БУВАЄ, ЯК НЕ БУВАЄ І АБСОЛЮТНО СЛАБКИХ ОСОБИСТОСТЕЙ. " +
+                "ОДНАК ОДНАКОВО КОРИСНО ЗНАТИ ПРО ВСІ СТОРІНИ ЛЮДИНИ."}
+            />
+            <BackgroundIcon
+              className="bg-emoticon_06"
+              caption="ТИП ПОВЕДІНКИ"
+              description={"КОНСТИТУЦІЯ ЛЮДИНИ ВИЗНАЧАЄ ЙОГО ТЕМПЕРАМЕНТ В ЦІЛОМУ ТА " +
+                "ПЕРЕВАЖНІ МОДЕЛІ ЙОГО ПОВЕДІНКИ."}
+            />
+          </div>
+          <div className="qualities-icon-pair-container">
+            <BackgroundIcon
+              className="bg-pazzle_14"
+              caption="ВЛАСТИВОСТІ КОНСТИТУЦІЇ ТА РЕАКЦІЇ НА РІЗНОМАНІТНІ СТИМУЛИ"
+              description={"ЛЮДИ З РІЗНИМИ ТИПАМИ КОНСТИТУЦІЇ НА ОДИН ПОДРАЗНИК РЕАГУЮТЬ ПО-РІЗНОМУ."}
+            />
+            <BackgroundIcon
+              className="bg-people_15"
+              caption="СХИЛЬНОСТІ ДО ФІЗИЧНИХ ТА ПСИХІЧНИХ ПРОБЛЕМ"
+              description={"ТИП КОНСТИТУЦІЇ ВИЗНАЧАЄ НАЙБІЛЬШ ЙМОВІРНІ ВАРІАНТИ ТІЛЕСНИХ І ПСИХОЛОГІЧНИХ " +
+                "ПРОБЛЕМ. ЦЕ ВАЖЛИВО ДЛЯ ЇХ ПРОФІЛАКТИКИ."}
+            />
+          </div>
+        </div>
       </div>
     </>
   );
